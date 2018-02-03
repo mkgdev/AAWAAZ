fd7e4a9bf14373fec1eb1f25770fbf105e4d21f9
sandeepyadav097 <sandeepyadav097@gmail.com>
2018-02-03 20:08:31 +0530

    Server setup

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app =    express();
+var bodyParser = require('body-parser');
+var indexRoutes = require('./routes/index.js');
+
+// Configuration
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+
+//routes configuration
+
+app.use('/',indexRoutes);
+
+
+
+
+//==========================
+// Server config
+//==========================
+
+app.listen(process.env.PORT||8000, function()
+{
+    console.log('Server listening on 8000')
+}
+);
+//===============================
